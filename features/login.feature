887aedc7b33b78df86cc823b2e10aff1f7dc61ca
Grigory Vlasov <ludo4ka.th@mail.ru>
2023-09-14 13:59:17 +0700

    add login steps file

--- features/login.feature
+++ features/login.feature
@@ -1,18 +1,20 @@
 Feature: Login Functionality
 
   Scenario: Login with valid credentials
-    Given I got navigated to login page
-    When I enter in "Email" with "test@test.com"
-    And I enter in "Password" with "test123pass"
+    Given I launch chrome browser
+    When I got navigated to login page
+    And I enter in Email with "ludo4ka.th@mail.ru"
+    And I enter in Password with "test12345!"
     And I press "Login"
     Then I should be in loggen in url
     Then I should see logged in user icon
     And I should see "Logout" button
 
-  Scenario: Login with invalid credentials
-    Given I got navigated to login page
-    When I enter in "Email" with "test@test.com"
-    And I enter in "Password" with "test123pass"
-    And I press "Login"
-    Then I should see error message
+#  Scenario: Login with invalid credentials
+#    Given I launch chrome browser
+#    When I got navigated to login page
+#    When I enter in "Email" with "test@test.com"
+#    And I enter in "Password" with "test123pass"
+#    And I press "Login"
+#    Then I should see error message
     And I should see "Logout" button
--- features/steps/login.py
+++ features/steps/login.py
@@ -1,34 +1,51 @@
 from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 
-@given('I got navigated to login page')
-def step_impl(context):
-    pass
+@given('I launch chrome browser')
+def open_browser(context):
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
 
-@when('I enter in "Email" with "test@test.com"')
-def step_impl(context):
-    pass
 
-@when('I enter in "Password" with "test123pass"')
-def step_impl(context):
-    pass
+@when('I got navigated to login page')
+def open_login_page(context):
+    context.driver.get("https://www.beerwulf.com/en-nl/my-account/Login?ReturnUrl=/en-NL/AccountInformation/OrderHistory")
+
+
+@when('I enter in Email with "{email}"')
+def enter_email(context, email):
+    username_locator = context.driver.find_element(By.CSS_SELECTOR, "[name='Email']")
+    username_locator.send_keys(email)
+
+
+@when('I enter in Password with "{password}"')
+def enter_password(context, password):
+    password_locator = context.driver.find_element(By.CSS_SELECTOR, "[name='Password']")
+    password_locator.send_keys(password)
+
 
 @when('I press "Login"')
-def step_impl(context):
-    pass
+def press_log_in(context):
+    submit_button_locator = context.driver.find_element(By.XPATH, "//button[text()='Log in']")
+    submit_button_locator.click()
+    time.sleep(2)
+
 
 @then('I should be in loggen in url')
 def step_impl(context):
     pass
 
 @then('I should see logged in user icon')
-def step_impl(context):
+def is_user_logged_in_icon_displayed(context):
     pass
 
 @then('I should see "Logout" button')
-def step_impl(context):
+def is_logout_button_displayed(context):
     pass
-
-@then('I should see error message')
-def step_impl(context):
-    pass
+#
+# @then('I should see error message')
+# def step_impl(context):
+#     pass
